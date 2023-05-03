21b6d4672143f5d06072f6b7258c7e1673f160df
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-03 15:23:56 +0200

    Create Item model

--- models/Item.model.js
+++ models/Item.model.js
@@ -1,7 +1,28 @@
 const { Schema, model } = require("mongoose");
 
+const categoryEnum = require('../services/categoryEnum');
+
 const itemSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: [true, 'Name is required.'],
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default:"No image"
+    },
+    category: {
+      type: String,
+      enum: Object.values(categoryEnum),
+    required: true,
+
+
+
 
   },
   {
