28e50d4c12c9d2a681483a981892f0bc3ac37c96
Nana Chuto <mail@nanachuto.com>
2023-05-05 10:35:23 +0200

    Update Cloudinary directory name

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -7,16 +7,16 @@ const multer = require("multer");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     allowed_formats: ["jpg", "png"],
-    folder: "movie-gallery" // The name of the folder in cloudinary
+    folder: "useum-uploads", // The name of the folder in cloudinary
     // resource_type: "raw", // => this is in case you want to upload other types of files, not just images
-  }
+  },
 });
 
 module.exports = multer({ storage });
