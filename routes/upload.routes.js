903f034ca30cbada20871605e656dcfa098c0cb8
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-06 12:53:41 +0200

    Move upload to own route file

---
+++ routes/upload.routes.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const fileUploader = require("../config/cloudinary.config");
+
+
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+	 console.log("file is: ", req.file)
+
+	if (!req.file) {
+	  next(new Error("No file uploaded!"));
+	  return;
+	}
+
+	// Get the URL of the uploaded file and send it as a response.
+	// 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+
+	res.json({ fileUrl: req.file.path });
+  });
+
+module.exports = router;
