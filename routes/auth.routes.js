9bc74c6f10840b4c0f41c5b08ccb8770319ed840
Nana Chuto <mail@nanachuto.com>
2023-05-07 08:38:07 +0200

    Improved wording for auth.routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,101 +1,118 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User.model');
-const { isAuthenticated } = require('../middleware/jwt.middleware');
-
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 const router = express.Router();
 const saltRounds = 10;
 
-router.post('/signup', async (req, res, next) => {
-	const { email, password, username, imageUrl } = req.body;
+router.post("/signup", async (req, res, next) => {
+  const { email, password, username, imageUrl } = req.body;
 
-	try {
-		if (email === '' || password === '' || username === '') {
-			res.status(400).json({ message: 'Provide email, password and username' });
-			return;
-		}
+  try {
+    if (email === "" || password === "" || username === "") {
+      res.status(400).json({ message: "Please fill out all fields" });
+      return;
+    }
 
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-		if (!emailRegex.test(email)) {
-			res.status(400).json({ message: 'Provide a valid email address.' });
-			return;
-		}
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: "Please provide a valid email address" });
+      return;
+    }
 
-		// Use regex to validate the password format
-		const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?\-]).{8,}/;
-		if (!passwordRegex.test(password)) {
-			res.status(400).json({
-				message:
-					'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.',
-			});
-		}
+    // Use regex to validate the password format
+    const passwordRegex =
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?\-]).{8,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      });
+    }
 
-		const foundUser = await User.findOne({ email });
+    const foundUser = await User.findOne({ email });
 
-		if (foundUser) {
-			res.status(400).json({ message: 'User already exists.' });
-			return;
-		}
+    if (foundUser) {
+      res.status(400).json({
+        message: "A user with this email address already exists",
+      });
+      return;
+    }
 
-		const salt = bcrypt.genSaltSync(saltRounds);
-		const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-		const createdUser = await User.create({ email, password: hashedPassword, username, imageUrl });
+    const createdUser = await User.create({
+      email,
+      password: hashedPassword,
+      username,
+      imageUrl,
+    });
 
-		console.log(createdUser)
+    console.log(createdUser);
 
-		const { email: createdEmail, username: createdName, _id: createdId, imageUrl: createdImageUrl } = createdUser;
+    const {
+      email: createdEmail,
+      username: createdName,
+      _id: createdId,
+      imageUrl: createdImageUrl,
+    } = createdUser;
 
-		const user = { email: createdEmail, username: createdName, _id: createdId, imageUrl: createdImageUrl };
+    const user = {
+      email: createdEmail,
+      username: createdName,
+      _id: createdId,
+      imageUrl: createdImageUrl,
+    };
 
-		res.status(201).json({ user });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'Internal Server Error' });
-		next(error);
-	}
+    res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
+  }
 });
 
-router.post('/login', async (req, res, next) => {
-	const { email, password } = req.body;
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
 
-	try {
-		if (email === '' || password === '') {
-			res.status(400).json({ message: 'Provide email and password.' });
-		}
+  try {
+    if (email === "" || password === "") {
+      res.status(400).json({ message: "Please fill out all fields" });
+    }
 
-		const foundUser = await User.findOne({ email });
+    const foundUser = await User.findOne({ email });
 
-		if (!foundUser) {
-			res.status(400).json({ message: 'User not found.' });
-		}
+    if (!foundUser) {
+      res.status(400).json({ message: "User not found." });
+    }
 
-		const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+    const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
-		if (passwordCorrect) {
-			const { _id, email, username } = foundUser;
-			const payload = { _id, email, username };
+    if (passwordCorrect) {
+      const { _id, email, username } = foundUser;
+      const payload = { _id, email, username };
 
-			const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
-				algorithm: 'HS256',
-				expiresIn: '6h',
-			});
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "6h",
+      });
 
-			res.status(200).json({ authToken });
-		} else {
-			res.status(401).json({ message: 'Unable to authenticate the user' });
-		}
-	} catch (error) {
-		res.status(500).json({ message: 'Internal Server Error' });
-	}
+      res.status(200).json({ authToken });
+    } else {
+      res.status(401).json({ message: "Unable to authenticate the user" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.get('/verify', isAuthenticated, (req, res, next) => {
-    console.log(`req.payload`, req.payload);
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  console.log(`req.payload`, req.payload);
 
-    res.status(200).json(req.payload);
+  res.status(200).json(req.payload);
 });
 
 module.exports = router;
