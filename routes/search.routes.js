945c7c13870c51abddf0a2b1f7ccc708c5533675
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-19 16:17:06 +0200

    add local db search capability

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -9,82 +9,118 @@ const Item = require("../models/Item.model");
 
 
 
+// router.get("/search", async (req, res, next) => {
+//     try {
+//         const { search } = req.query;
+//         let collections;
+//         let items;
+//         let users;
+
+//         if (search) {
+//           console.log("SEARCHING FOR: ", search)
+//             collections = await Collection.aggregate([{
+//                 '$search': {
+//                   'index': 'CollectionSearchIndex',
+//                   'text': {
+//                     'query': search,
+//                     'path': 'name'
+//                   }
+//                 }
+//               }, {
+//                 '$limit': 3
+//               }
+//             ]);
+
+//             items = await Item.aggregate([
+//                 {
+//                   '$search': {
+//                     'index': 'ItemSearchIndex',
+//                     'text': {
+//                       'query': search,
+//                       'path': 'name'
+//                     }
+//                   }
+//                 }, {
+//                   '$limit': 3
+//                 }, {
+//                   '$project': {
+//                     '_id': 1,
+//                     'name': 1,
+//                     'imageUrl': 1
+//                   }
+//                 }
+//               ]);
+
+//             users = await User.aggregate([
+//                 {
+//                   '$search': {
+//                     'index': 'UserSearchIndex',
+//                     'text': {
+//                       'query': search,
+//                       'path': 'username'
+//                     }
+//                   }
+//                 }, {
+//                   '$limit': 3
+//                 }, {
+//                   '$project': {
+//                     '_id': 1,
+//                     'username': 1,
+//                     'imageUrl': 1
+//                   }
+//                 }
+//               ]);
+
+
+//         } else {
+//             collections = await Collection.find().sort({ name: 'asc' }).limit(10);
+//             items = await Item.find().sort({ name: 'asc' }).limit(10);
+//         }
+
+//         res.status(200).json({ collections, items, users });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Internal Server Error" });
+//         next(error);
+//     }
+// });
+
+
+
+
 router.get("/search", async (req, res, next) => {
-    try {
-        const { search } = req.query;
-        let collections;
-        let items;
-        let users;
+  try {
+    const { search } = req.query;
+    let collections;
+    let items;
+    let users;
 
-        if (search) {
-          console.log("SEARCHING FOR: ", search)
-            collections = await Collection.aggregate([{
-                '$search': {
-                  'index': 'CollectionSearchIndex',
-                  'text': {
-                    'query': search,
-                    'path': 'name'
-                  }
-                }
-              }, {
-                '$limit': 3
-              }
-            ]);
+    if (search) {
+      console.log("SEARCHING FOR: ", search);
 
-            items = await Item.aggregate([
-                {
-                  '$search': {
-                    'index': 'ItemSearchIndex',
-                    'text': {
-                      'query': search,
-                      'path': 'name'
-                    }
-                  }
-                }, {
-                  '$limit': 3
-                }, {
-                  '$project': {
-                    '_id': 1,
-                    'name': 1,
-                    'imageUrl': 1
-                  }
-                }
-              ]);
+      collections = await Collection.find({ $text: { $search: search } })
+        .limit(3)
+        .select("name");
 
-            users = await User.aggregate([
-                {
-                  '$search': {
-                    'index': 'UserSearchIndex',
-                    'text': {
-                      'query': search,
-                      'path': 'username'
-                    }
-                  }
-                }, {
-                  '$limit': 3
-                }, {
-                  '$project': {
-                    '_id': 1,
-                    'username': 1,
-                    'imageUrl': 1
-                  }
-                }
-              ]);
+      items = await Item.find({ $text: { $search: search } })
+        .limit(3)
+        .select("_id name imageUrl");
 
-
-        } else {
-            collections = await Collection.find().sort({ name: 'asc' }).limit(10);
-            items = await Item.find().sort({ name: 'asc' }).limit(10);
-        }
-
-        res.status(200).json({ collections, items, users });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
-        next(error);
+      users = await User.find({ $text: { $search: search } })
+        .limit(3)
+        .select("_id username imageUrl");
+    } else {
+      collections = await Collection.find().sort({ name: "asc" }).limit(10);
+      items = await Item.find().sort({ name: "asc" }).limit(10);
     }
-});
 
+    res.status(200).json({ collections, items, users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
+  }
+});
 
 
 
