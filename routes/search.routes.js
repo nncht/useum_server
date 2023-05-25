2fd2e9ff113fec37a10868bd11c6e3f670125e7b
Nana Chuto <mail@nanachuto.com>
2023-05-26 00:49:18 +0200

    Extend search result limit to 4

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -6,91 +6,84 @@ const User = require("../models/User.model");
 const Category = require("../models/Category.model");
 const Item = require("../models/Item.model");
 
-
-
-
 router.get("/search", async (req, res, next) => {
-    try {
-        const { search } = req.query;
-        let collections;
-        let items;
-        let users;
+  try {
+    const { search } = req.query;
+    let collections;
+    let items;
+    let users;
 
-        if (search) {
-          console.log("SEARCHING FOR: ", search)
-            collections = await Collection.aggregate([{
-                '$search': {
-                  'index': 'CollectionSearchIndex',
-                  'text': {
-                    'query': search,
-                    'path': 'name'
-                  }
-                }
-              }, {
-                '$limit': 3
-              }
-            ]);
+    if (search) {
+      console.log("SEARCHING FOR: ", search);
+      collections = await Collection.aggregate([
+        {
+          $search: {
+            index: "CollectionSearchIndex",
+            text: {
+              query: search,
+              path: "name",
+            },
+          },
+        },
+        {
+          $limit: 4,
+        },
+      ]);
 
-            items = await Item.aggregate([
-                {
-                  '$search': {
-                    'index': 'ItemSearchIndex',
-                    'text': {
-                      'query': search,
-                      'path': 'name'
-                    }
-                  }
-                }, {
-                  '$limit': 3
-                }, {
-                  '$project': {
-                    '_id': 1,
-                    'name': 1,
-                    'imageUrl': 1
-                  }
-                }
-              ]);
+      items = await Item.aggregate([
+        {
+          $search: {
+            index: "ItemSearchIndex",
+            text: {
+              query: search,
+              path: "name",
+            },
+          },
+        },
+        {
+          $limit: 4,
+        },
+        {
+          $project: {
+            _id: 1,
+            name: 1,
+            imageUrl: 1,
+          },
+        },
+      ]);
 
-            users = await User.aggregate([
-                {
-                  '$search': {
-                    'index': 'UserSearchIndex',
-                    'text': {
-                      'query': search,
-                      'path': 'username'
-                    }
-                  }
-                }, {
-                  '$limit': 3
-                }, {
-                  '$project': {
-                    '_id': 1,
-                    'username': 1,
-                    'imageUrl': 1
-                  }
-                }
-              ]);
-
-
-        } else {
-            collections = await Collection.find().sort({ name: 'asc' }).limit(10);
-            items = await Item.find().sort({ name: 'asc' }).limit(10);
-        }
-
-        res.status(200).json({ collections, items, users });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
-        next(error);
+      users = await User.aggregate([
+        {
+          $search: {
+            index: "UserSearchIndex",
+            text: {
+              query: search,
+              path: "username",
+            },
+          },
+        },
+        {
+          $limit: 4,
+        },
+        {
+          $project: {
+            _id: 1,
+            username: 1,
+            imageUrl: 1,
+          },
+        },
+      ]);
+    } else {
+      collections = await Collection.find().sort({ name: "asc" }).limit(10);
+      items = await Item.find().sort({ name: "asc" }).limit(10);
     }
+
+    res.status(200).json({ collections, items, users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
+  }
 });
 
-
-
-
-
-
-
-
-
-  module.exports = router;
+module.exports = router;
