1a435efb5554cff27a0f4660a00e330881fbdd9b
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-26 17:30:06 +0200

    Update trial 2

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: "https://useum.netlify.app",
+      origin: FRONTEND_URL,
     })
   );
 
