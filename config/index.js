175d7373a540401eed1464a7a7d93e2910ccd807
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-27 08:27:08 +0200

    Update

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: FRONTEND_URL
+      origin: FRONTEND_URL,
     })
   );
 
