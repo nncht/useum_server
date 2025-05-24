39968cdc48b8a4894f0d793a3b265f878e4b7168
Nana Chuto <mail@nanachuto.com>
2025-05-25 01:28:22 +0700

    Fixing URI

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
-// const FRONTEND_URL = "https://useum.netlify.app";
+// const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
+const FRONTEND_URL = "https://useum.netlify.app";
 
 // Middleware configuration
 module.exports = (app) => {
